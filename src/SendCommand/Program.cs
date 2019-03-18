83df98c62f5d21a9198acf327c62b7e783d8f406
wk-j <somnuk.wk@gmail.com>
2019-03-18 20:27:30 +0700

    Initial commit

---
+++ src/SendCommand/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SendCommand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
